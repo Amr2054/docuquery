1bde6bcbf92f606055cd38a59865e966be87790d
amr <amrahmedgohar205@gmail.com>
2026-06-17 03:47:30 +0300

    Create Vector database factory & Used qdrant as Provider

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from stores.llm.LLMProviderFactory import LLMProviderFactory
 app = FastAPI()
 
 # adding the db_client to the app on startup
-@app.on_event("startup")
+
 async def startup_db_client():
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
@@ -22,10 +22,13 @@ async def startup_db_client():
     app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
                                              embedding_size=settings.EMBEDDING_MODEL_SIZE)
-    
-@app.on_event("shutdown")
+
+
 async def shutdown_db_client():
     app.mongo_conn.close()
+
+app.router.lifespan.on_startup.append(startup_db_client)
+app.router.lifespan.on_shutdown.append(shutdown_db_client)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
